2f7a780bcd57d816ca2a9d5acc4311f4c3a13b41
LESLIE MORALES <47612647+lesliemoralesc1@users.noreply.github.com>
2021-09-13 13:57:50 -0500

    Update funciones.py
     Agregué la función calcular_area_rombo (Diagonal,diagonal1 ) por Leslie Morales

--- funciones.py
+++ funciones.py
@@ -5,8 +5,10 @@ def calcular_area_cuadrado (lado):
 def calcular_volumen_paralelepipedo(base,altura, alto):
 	volumen=base*altura*alto
 	return volumen
-	
-	
+
+def calcular_area_rombo (Diagonal,diagonal1 ):
+        area=((Diagonal*diagonal1)/2)
+        return area
 #print(f"El area del cuadrado es {calcular_area_cuadrado(4)}")
 
 	 
